dd4de80569b42ffde476bdd58bd2ec4ecc5a459c
Ben Rady <benrady@gmail.com>
2018-01-21 13:51:09 -0600

    Serial version

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+extern crate num;
+extern crate image;
+
+use num::Complex;
+use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+use std::io::Result;
+use std::io::Write;
+
+fn write_image(filename : &str, 
+               pixels : &[u8], 
+               bounds: (usize, usize)) -> Result<()> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
+}
+
+fn escape_time(c : Complex<f64>, limit : u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        z = z*z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn parse_pair<T: FromStr>(s : &str, separator : char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+    
+}
+
+fn render(pixels: &mut [u8], 
+          bounds: (usize, usize), 
+          upper_left: Complex<f64>, 
+          lower_right: Complex<f64>) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+    for row in 0 .. bounds.1 {
+        for column in 0 .. bounds.0 {
+            let point = pixel_to_point(bounds, 
+                                       (column, row),
+                                       upper_left,
+                                       lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            }
+        }
+    }
+}
+
+fn pixel_to_point(bounds: (usize, usize), 
+                  pixel: (usize, usize), 
+                  upper_left: Complex<f64>, 
+                  lower_right: Complex<f64>) -> Complex<f64> {
+    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+
+fn parse_complex(s : &str) -> Option<Complex<f64>> {
+    match parse_pair::<f64>(s, ',') {
+        Some((re, im)) => { Some(Complex { re, im }) },
+        None => None
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 5 {
+        writeln!(std::io::stderr(), "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT").unwrap();
+        std::process::exit(1);
+    }
+    let bounds = parse_pair(&args[2], 'x').expect("Error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("Error parsing UPPERLEFT");
+    let lower_right = parse_complex(&args[4]).expect("Error parsing LOWERRIGHT");
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+    render(&mut pixels, bounds, upper_left, lower_right);
+    write_image(&args[1], &pixels, bounds).expect("Error writing image")
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), 
+                              (25, 75), 
+                              Complex {re: -1.0, im: 1.0},
+                              Complex {re: 1.0, im: -1.0}), 
+               Complex { re: -0.5, im: -0.5});
+    
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex {re: 1.25, im: -0.0625}));
+}
+
+#[test]
+fn test_parse_pair() {
+    let pair: Option<(i32, i32)> = Some((10, 20));
+    let none: Option<(i32, i32)> = None;
+    assert_eq!(parse_pair::<u32>("", ','), None);
+    assert_eq!(parse_pair("10,", ','), none);
+    assert_eq!(parse_pair(",10", ','), none);
+    assert_eq!(parse_pair("10,20", ','), pair);
+    assert_eq!(parse_pair("10xy,20", ','), none);
+    assert_eq!(parse_pair("10x20", 'x'), pair);
+}
+
+#[test]
+fn test_no_escape() {
+    let time = escape_time(Complex {re: 0.0, im: 0.0}, 1);
+    assert_eq!(time, Option::None);
+}

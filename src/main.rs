b2069cdb2e2b5337e0bcb4fab8324c5649c8b619
Ben Rady <benrady@gmail.com>
2018-01-21 14:19:51 -0600

    Parallel version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate num;
 extern crate image;
+extern crate crossbeam;
 
 use num::Complex;
 use std::str::FromStr;
@@ -80,6 +81,29 @@ fn parse_complex(s : &str) -> Option<Complex<f64>> {
     }
 }
 
+fn render_parallel(pixels: &mut [u8], 
+                   bounds: (usize, usize), 
+                   upper_left: Complex<f64>, 
+                   lower_right: Complex<f64>) {
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 5 {
@@ -90,7 +114,7 @@ fn main() {
     let upper_left = parse_complex(&args[3]).expect("Error parsing UPPERLEFT");
     let lower_right = parse_complex(&args[4]).expect("Error parsing LOWERRIGHT");
     let mut pixels = vec![0; bounds.0 * bounds.1];
-    render(&mut pixels, bounds, upper_left, lower_right);
+    render_parallel(&mut pixels, bounds, upper_left, lower_right);
     write_image(&args[1], &pixels, bounds).expect("Error writing image")
 }
 
